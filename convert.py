c247680ea1028da2fe2a42559cdcfb809643cbf2
Denmark <dengarcia.x@gmail.com>
2018-03-27 21:13:14 +0800

    Added README.md

--- convert.py
+++ convert.py
@@ -4,14 +4,12 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--celsius', help="convert farenheit to celsius")
 parser.add_argument('--farenheit', help="convert celsius to farenheit")
-
 args = parser.parse_args()
 
-x = float(args.farenheit)
 
 if args.celsius:
 	cel = convert_F(float(args.celsius))
 	print(cel, 'F')
 elif args.farenheit:
-	far = convert_C(x)
+	far = convert_C(float(args.farenheit))
 	print(far,'C')

2064cc566303c414239d18ab2741defed47225e0
Denmark <dengarcia.x@gmail.com>
2018-03-28 13:06:31 +0800

    Add ability to delete folder

--- argv.py
+++ argv.py
@@ -8,12 +8,13 @@ Usage: python argv.py [--username], [--password], [--help]
 --username				username
 --password				password
 --create, -c 			create a folder
+--delete, -d 			delete a foler
 
 	''')
 
 
 def create(folder):
-	name = os.mkdir('/home/denmark/Desktop/pythontutorial/%s' %(folder))
+	name = os.mkdir('/home/denmark/Desktop/pythontutorial/%s' %(folder), mode = 0o000)
 
 
 
@@ -26,5 +27,11 @@ if __name__ == '__main__':
 			print("This folder is already exist!")
 		else:
 			create(sys.argv[2])
+	elif(sys.argv[1] == '--delete' or sys.argv[1] == '-d'):
+		try:
+			if(os.rmdir(sys.argv[2])):
+				print("Deleted directory %s" % sys.argv[2])
+		except FileNotFoundError:
+			print("File was not found or have been already deleted!")
 	else:
 		print('Flag is not recognized, Kindly type --help for available options')

1b72c9be8fc5146635824234c4d3152a5ce6ef73
Denmark <dengarcia.x@gmail.com>
2018-03-28 12:43:16 +0800

    Check if directory was created, if created it print error

--- argv.py
+++ argv.py
@@ -22,7 +22,9 @@ if __name__ == '__main__':
 	if(sys.argv[1] == '--help' or sys.argv[1] == '-h'):
 			print(help())
 	elif(sys.argv[1] == '--create' or sys.argv[1] == '-c'):
-
-		create(sys.argv[2])
+		if(os.path.exists(sys.argv[2])):
+			print("This folder is already exist!")
+		else:
+			create(sys.argv[2])
 	else:
 		print('Flag is not recognized, Kindly type --help for available options')

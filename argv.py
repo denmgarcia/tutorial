224f5def9c4a6bd9a7544f7889585116f6652037
Denmark <dengarcia.x@gmail.com>
2018-03-28 12:04:19 +0800

    Script creates a folder using os.mkdir in the folder specified location

---
+++ argv.py
@@ -0,0 +1,28 @@
+import sys, os
+
+def help():
+	return ('''
+Usage: python argv.py [--username], [--password], [--help]
+
+--help, -h				show list of help
+--username				username
+--password				password
+--create, -c 			create a folder
+
+	''')
+
+
+def create(folder):
+	name = os.mkdir('/home/denmark/Desktop/pythontutorial/%s' %(folder))
+
+
+
+
+if __name__ == '__main__':
+	if(sys.argv[1] == '--help' or sys.argv[1] == '-h'):
+			print(help())
+	elif(sys.argv[1] == '--create' or sys.argv[1] == '-c'):
+
+		create(sys.argv[2])
+	else:
+		print('Flag is not recognized, Kindly type --help for available options')
